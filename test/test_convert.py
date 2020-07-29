bc50e9e91ba2bb6769079101b07ab1274b7d2938
Bryan Eikema <bryaneikema@gmail.com>
2020-07-29 10:31:33 +0200

    additional test for convert_from_fairseq

--- test/test_convert.py
+++ test/test_convert.py
@@ -6,6 +6,19 @@ from mbr_nmt.convert import convert_from_fairseq
 class TestConvert(unittest.TestCase):
 
     def test_convert_from_fairseq(self):
+
+        # Test with a single input file.
+        tmp = tempfile.TemporaryFile(mode="w+")
+        try:
+            convert_from_fairseq(["test/data/fairseq/samples-2_1.en"], 
+                                 tmp, verbose=False)
+            tmp.seek(0)
+            result = tmp.readlines()
+            with open("test/data/samples-2.en", "r") as f: expected = f.readlines()
+            self.assertEqual(result, expected)
+        finally: tmp.close()
+
+        # Test with multiple input files.
         tmp = tempfile.TemporaryFile(mode="w+")
         try:
             convert_from_fairseq(["test/data/fairseq/samples-2_1.en", "test/data/fairseq/samples-2_2.en"], 
